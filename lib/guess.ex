de2272cbfa22874e5fc9ff7216c0d22b25d02091
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-07-12 08:14:38 -0300

    :sparkles: Implementing Pattern matching and method get range with input

--- lib/guess.ex
+++ lib/guess.ex
@@ -1,12 +1,42 @@
 defmodule Guess do
   use Application
 
-  def start(_,_) do
+  def start(_, _) do
     run()
     {:ok, self()}
   end
 
   def run() do
     IO.puts("Let's play Guess the Number")
+
+    IO.gets("Pick a difficulty level: (1) Easy, (2) Medium, (3) Hard: ")
+    |> Integer.parse()
+    |> parse_input()
+    |> get_range()
+    |> IO.inspect()
+  end
+
+  def parse_input(:error) do
+    IO.puts("\nInvalid Input!!!\n")
+    run()
+  end
+
+  def parse_input({num, _}), do: num
+
+  def get_range(level) do
+    case level do
+      1 ->
+        1..10
+
+      2 ->
+        1..100
+
+      3 ->
+        1..1000
+
+      _ ->
+        IO.puts("\nInvalid level!!!\n")
+        run()
+    end
   end
 end
